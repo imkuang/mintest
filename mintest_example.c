32f590756fbaf8b04596687cd5cc5bd126c9cc7b
Ming Kuang <ming@imkuang.com>
2024-03-29 23:19:39 +0800

    directly end the current testcase when assert fail
    实际测试使用场景下，同一测试用例内的不同断言会存在关联或影响，不应
    在已有断言检测失败的情况下继续执行后续用例逻辑，典型的如先检测指针
    地址要求其不为空，再检测该指针指向内存存储的值的情况，若前者失败后
    还继续执行后者可能会导致空指针解引用挂起。
    
    这里修改为所有的测试断言宏接口加上在测试失败时return结束当前测试用
    例的逻辑，每个用例仅打印首个检查失败的断言提示消息（后续断言实际也
    未被执行）。

--- mintest_example.c
+++ mintest_example.c
@@ -3,14 +3,14 @@
 static int test_int_value = 0;
 static const char *test_string = "a test string";
 
-/* 待测试函数1，返回两个int型数字和全局变量test_int_value的和，并更新test_int_value的值加1 */
+/* 待测试函数1，计算两个int型数字和全局变量test_int_value的和，并更新test_int_value的值加1 */
 int my_add_int(int a, int b)
 {
     int ret = (a + b + test_int_value);
     test_int_value += 1;
     return ret;
 }
-/* 待测试函数2，返回两个double型数字和全局变量test_int_value的和，并更新test_int_value的值加1 */
+/* 待测试函数2，计算两个double型数字和全局变量test_int_value的和，并更新test_int_value的值加1 */
 double my_add_double(double a, double b)
 {
     double ret = (a + b + (double)test_int_value);
@@ -43,10 +43,10 @@ MT_TESTCASE(test_assert_pass)
 MT_TESTCASE(test_assert_fail)
 {
     /* 同上，test_int_value先被初始化为1，后续每次my_add_int调用会使其加1，因此预期第一个断言通过，后续失败 */
-    /* 同一测试用例中有任一断言失败则判定整个测试用例失败 */
+    /* 同一测试用例中有任一断言失败则判定整个测试用例失败，后续逻辑不会再执行 */
     mt_assert(my_add_int(1, 1) == 3, "my_add_int(1, 1) calc result should be 3"); /* PASS */
     mt_assert(my_add_int(1, 1) == 3, "my_add_int(1, 1) calc result should be 4"); /* FAIL */
-    mt_assert(my_add_int(1, 1) == 3, "my_add_int(1, 1) calc result should be 5"); /* FAIL */
+    mt_assert(my_add_int(1, 1) == 3, "my_add_int(1, 1) calc result should be 5"); /* NOT RUN */
 }
 
 /* 定义测试用例test_assert_int_eq_pass，验证mt_assert_int_eq断言测试成功的场景 */
@@ -62,7 +62,7 @@ MT_TESTCASE(test_assert_int_eq_fail)
 {
     mt_assert_int_eq(3, my_add_int(1, 1)); /* PASS */
     mt_assert_int_eq(3, my_add_int(1, 1)); /* FAIL */
-    mt_assert_int_eq(3, my_add_int(1, 1)); /* FAIL */
+    mt_assert_int_eq(3, my_add_int(1, 1)); /* NOT RUN */
 }
 
 /* 定义测试用例test_assert_double_eq_pass，验证mt_assert_double_eq断言测试成功的场景 */
@@ -78,7 +78,7 @@ MT_TESTCASE(test_assert_double_eq_fail)
 {
     mt_assert_double_eq(3.2, my_add_double(1.1, 1.1)); /* PASS */
     mt_assert_double_eq(3.2, my_add_double(1.1, 1.1)); /* FAIL */
-    mt_assert_double_eq(3.2, my_add_double(1.1, 1.1)); /* FAIL */
+    mt_assert_double_eq(3.2, my_add_double(1.1, 1.1)); /* NOT RUN */
 }
 
 /* 定义测试用例test_assert_string_eq_pass，验证mt_assert_string_eq断言测试成功的场景 */
@@ -90,25 +90,8 @@ MT_TESTCASE(test_assert_string_eq_pass)
 /* 定义测试用例test_assert_string_eq_fail，验证mt_assert_string_eq断言测试失败的场景 */
 MT_TESTCASE(test_assert_string_eq_fail)
 {
-    mt_assert_string_eq("a test string", test_string);  /* PASS */
-    mt_assert_string_eq("a test string1", test_string); /* FAIL */
-    mt_assert_string_eq(NULL, test_string);             /* FAIL */
-}
-
-/* 定义测试用例test_fail_message_exceeds_max_num，验证测试失败消息数超过缓存最大值的情况 */
-MT_TESTCASE(test_fail_message_exceeds_max_num)
-{
-    /* 默认每个用例只缓存输出8条消息，这里最后两条消息会被截断 */
-    mt_assert_int_eq(2, my_add_int(1, 1)); /* FAIL */
-    mt_assert_int_eq(2, my_add_int(1, 1)); /* FAIL */
-    mt_assert_int_eq(2, my_add_int(1, 1)); /* FAIL */
-    mt_assert_int_eq(2, my_add_int(1, 1)); /* FAIL */
-    mt_assert_int_eq(2, my_add_int(1, 1)); /* FAIL */
-    mt_assert_int_eq(2, my_add_int(1, 1)); /* FAIL */
-    mt_assert_int_eq(2, my_add_int(1, 1)); /* FAIL */
-    mt_assert_int_eq(2, my_add_int(1, 1)); /* FAIL */
-    mt_assert_int_eq(2, my_add_int(1, 1)); /* FAIL */
-    mt_assert_int_eq(2, my_add_int(1, 1)); /* FAIL */
+    mt_assert_string_eq("a test string", test_string); /* PASS */
+    mt_assert_string_eq("hello world", test_string);   /* FAIL */
 }
 
 /* 组合上述一系列测试用例，定义测试套件test_suite1 */
@@ -126,7 +109,6 @@ MT_TESTSUITE(test_suite1)
     MT_RUN_TESTCASE(test_assert_double_eq_fail);
     MT_RUN_TESTCASE(test_assert_string_eq_pass);
     MT_RUN_TESTCASE(test_assert_string_eq_fail);
-    MT_RUN_TESTCASE(test_fail_message_exceeds_max_num);
 }
 
 /* 不配置用例准备和清理函数，组合部分测试用例定义测试套件test_suite2 */
@@ -136,8 +118,8 @@ MT_TESTSUITE(test_suite2)
     test_int_value = 1;
 
     /* 每个用例执行前test_int_value的值不会再被重置，因此用例之间会互相影响 */
-    MT_RUN_TESTCASE(test_assert_int_eq_pass); /* 预期全部成功，因为test_int_value初始值为1 */
-    MT_RUN_TESTCASE(test_assert_int_eq_pass); /* 预期全部失败，因为test_int_value初始值为4 */
+    MT_RUN_TESTCASE(test_assert_int_eq_pass); /* 预期成功，因为test_int_value初始值为1 */
+    MT_RUN_TESTCASE(test_assert_int_eq_pass); /* 预期失败，因为test_int_value初始值为4 */
 }
 
 int main(void)
